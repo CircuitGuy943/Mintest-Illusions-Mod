462a20082fbe13cac508118217a120dcea9e8ea9
CircuitGuy943 <102479222+CircuitGuy943@users.noreply.github.com>
2022-03-28 15:43:42 +0100

    illusion(2)

--- illusion/init.lua
+++ illusion/init.lua
@@ -14,20 +14,80 @@ minetest.register_node("illusion:blue_supernova", {
 	},
 	is_ground_content = true,
 	groups = {cracky = 3},
-	drop = 'illusion:illusional_shard 6'
+	drop = {"illusion:beaty_shard 1", "illusion:illusional_shard 8"}
+})
+
+minetest.register_node("illusion:square_wormhole", {
+	description = "Blow your mind with this mind-bending squared wormhole!",
+	tiles = {
+		"SquareWormhole.png",
+		"SquareWormhole.png",
+		"SquareWormhole.png",
+		"SquareWormhole.png",
+		"SquareWormhole.png",
+		"SquareWormhole.png"
+	},
+	is_ground_content = true,
+	groups = {cracky = 3},
+	drop = {"illusion:beaty_shard 8", "illusion:illusional_shard 1"}
+})
+
+minetest.register_node("illusion:quad_circle", {
+	description = "Blow your mind with this deep four circles",
+	tiles = {
+		"CirclingQuadtriple.png",
+		"CirclingQuadtriple.png",
+		"CirclingQuadtriple.png",
+		"CirclingQuadtriple.png",
+		"CirclingQuadtriple.png",
+		"CirclingQuadtriple.png"
+	},
+	is_ground_content = true,
+	groups = {cracky = 3},
+	drop = {"illusion:beaty_shard 8", "illusion:illusional_shard 1"}
+})
+
+minetest.register_node("illusion:stacked_squares", {
+	description = "Blow your mind with these squares in squares",
+	tiles = {
+		"StackedSquares.png",
+		"StackedSquares.png",
+		"StackedSquares.png",
+		"StackedSquares.png",
+		"StackedSquares.png",
+		"StackedSquares.png"
+	},
+	is_ground_content = true,
+	groups = {cracky = 3},
+	drop = {"illusion:beaty_shard 8", "illusion:illusional_shard 1"}
+})
+
+minetest.register_node("illusion:bendy_circles", {
+	description = "Blow your mind with these bent circles",
+	tiles = {
+		"OpticalYellowPurple.png",
+		"OpticalYellowPurple.png",
+		"OpticalYellowPurple.png",
+		"OpticalYellowPurple.png",
+		"OpticalYellowPurple.png",
+		"OpticalYellowPurple.png"
+	},
+	is_ground_content = true,
+	groups = {cracky = 3},
+	drop = {"illusion:beaty_shard 8", "illusion:illusional_shard 1"}
 })
 
 minetest.register_craftitem("illusion:illusional_shard", {
 	description = "Used to craft illusion blocks",
-	invemtory_image = "IllusionalShard.png"
+	inventory_image = "IllusionalShard.png"
 })
 
 minetest.register_craftitem("illusion:beaty_shard", {
 	description = "Used to craft illusion blocks",
-	invemtory_image = "BeatyShard.png"
+	inventory_image = "BeatyShard.png"
 })
 
-minetest.register_craftitem("illusion:dimond_pie", {
+minetest.register_craftitem("illusion:dimond_cake", {
 	description = "Dimond Pie, eat to get full health",
 	inventory_image = "BeatyCake.png",
 	on_use = minetest.item_eat(100)
@@ -36,27 +96,65 @@ minetest.register_craftitem("illusion:dimond_pie", {
 -- Define all crafting reciepies
 
 minetest.register_craft({
-	output = "illusion:blue_supernova 1",
+	output = "illusion:square_wormhole 9",
 	recipe = {
-		{"illusion:illusional_shard", "illusion:illusional_shard"},
-		{"illusion:illusional_shard", "illusion:illusional_shard"},
-		{"illusion:illusional_shard", "illusion:illusional_shard"}
+		{"illusion:illusional_shard", "illusion:illusional_shard", "illusion:illusional_shard"},
+		{"illusion:illusional_shard", "illusion:beaty_shard", "illusion:illusional_shard"},
+		{"illusion:illusional_shard", "illusion:illusional_shard", "illusion:illusional_shard"}
 	}
 })
 
 minetest.register_craft({
-	output = "illusion:illusional_shard 2",
-	type = "shapeless",
+	output = "illusion:blue_supernova 9",
 	recipe = {
-		{"default:diamondblock"}
+		{"illusion:beaty_shard", "illusion:beaty_shard", "illusion:beaty_shard"},
+		{"illusion:beaty_shard", "illusion:illusional_shard", "illusion:beaty_shard"},
+		{"illusion:beaty_shard", "illusion:beaty_shard", "illusion:beaty_shard"}
 	}
 })
 
 minetest.register_craft({
-	output = "illusion:beaty_shard 2",
-	type = "shapeless",
+	output = "illusion:quad_circle 9",
 	recipe = {
-		{"default:mese"}
+		{"illusion:beaty_shard", "illusion:illusional_shard", "illusion:beaty_shard"},
+		{"illusion:illusional_shard", "illusion:beaty_shard", "illusion:illusional_shard"},
+		{"illusion:beaty_shard", "illusion:illusional_shard", "illusion:beaty_shard"}
+	}
+})
+
+minetest.register_craft({
+	output = "illusion:stacked_squares 9",
+	recipe = {
+		{"illusion:beaty_shard", "illusion:beaty_shard", "illusion:beaty_shard"},
+		{"illusion:illusional_shard", "illusion:illusional_shard", "illusion:illusional_shard"},
+		{"illusion:beaty_shard", "illusion:beaty_shard", "illusion:beaty_shard"}
+	}
+})
+
+minetest.register_craft({
+	output = "illusion:bendy_circles 9",
+	recipe = {
+		{"illusion:illusional_shard", "illusion:beaty_shard", "illusion:illusional_shard"},
+		{"illusion:illusional_shard", "illusion:beaty_shard", "illusion:illusional_shard"},
+		{"illusion:illusional_shard", "illusion:beaty_shard", "illusion:illusional_shard"}
+	}
+})
+
+minetest.register_craft({
+	output = "illusion:illusional_shard 6",
+	recipe = {
+		{"", "", ""},
+		{"", "default:diamondblock", ""},
+		{"", "", ""}
+	}
+})
+
+minetest.register_craft({
+	output = "illusion:beaty_shard 6",
+	recipe = {
+		{"", "", ""},
+		{"", "default:mese", ""},
+		{"", "", ""}
 	}
 })
 
