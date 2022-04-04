7410498aad00af34f73331272713a89fe9eb59d7
CircuitGuy943 <102479222+CircuitGuy943@users.noreply.github.com>
2022-04-04 19:47:19 +0100

    Update init.lua

--- illusion/init.lua
+++ illusion/init.lua
@@ -4,7 +4,6 @@ dofile(default_path.."/nodes.lua") -- Finsished
 dofile(default_path.."/stairs.lua") -- Finsished
 dofile(default_path.."/slabs.lua") -- Finished
 dofile(default_path.."/craftitems.lua") -- Finsished
---dofile(default_path.."/hoverstairs.lua")
 
 print("This file will be run at load time!")
 
